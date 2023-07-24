0e31ccbe3c994aa6ed0b4e14f1b560a9ddcfc1a8
ycn999 <136255659+ycn999@users.noreply.github.com>
2023-07-24 08:25:19 +0800

    Create Q6.sql

---
+++ Q6.sql
@@ -0,0 +1,31 @@
+## Q6 A/B Test Analysis: difference in cancellation rate after implementing checkout page modification ##
+
+SELECT
+  ab_test_group,
+  yearmonth,
+  DeviceType,
+  channel_name,
+  SUM(IsCancelled) AS cancellation,
+  COUNT(DISTINCT(ItineraryItemID)) AS total_booking,
+  SUM(BookingValue) AS booking_value
+FROM
+  (
+  SELECT
+    ItineraryItemID,
+    FORMAT_DATE('%Y-%b', OrderDate) AS yearmonth,
+    DeviceType,
+    CASE WHEN ChannelID = 1949 THEN 'Ambiguous: Email/Other' ELSE ChannelName END AS channel_name,
+    CASE WHEN MOD(ItineraryItemID,2) = 0 THEN 'test' ELSE 'control' END AS ab_test_group,
+    IsCancelled,
+    BookingValue
+  FROM Bookings t1
+  INNER JOIN DeviceType t2
+   ON t1.DeviceID = t2.DeviceID
+  INNER JOIN ChannelName t3
+   ON t1.ChannelID = t3.Channel_ID
+  WHERE 
+    EXTRACT(MONTH FROM OrderDate) = 1 AND
+    EXTRACT(YEAR FROM OrderDate) = 2018
+  )
+GROUP BY ab_test_group, yearmonth, DeviceType, channel_name
+ORDER BY yearmonth, ab_test_group, DeviceType, channel_name
