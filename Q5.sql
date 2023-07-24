93ed53a8e4127228e43e13f7a5e59336a1dde727
ycn999 <136255659+ycn999@users.noreply.github.com>
2023-07-24 08:20:04 +0800

    Create Q5.sql

---
+++ Q5.sql
@@ -0,0 +1,10 @@
+## Q5 Forecast the number of bookings for October 2018. ##
+
+SELECT
+  EXTRACT(YEAR FROM OrderDate) AS year,
+  EXTRACT(MONTH FROM OrderDate) AS month,
+  FORMAT_DATE('%Y-%b', OrderDate) AS year_month,
+  COUNT(ItineraryItemID) AS booking_count
+FROM Bookings
+GROUP BY year, month, year_month
+ORDER BY year, month
