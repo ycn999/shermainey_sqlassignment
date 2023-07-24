f8cd5dccb95d251b48dfb39ee8b82a76eefdf9b9
ycn999 <136255659+ycn999@users.noreply.github.com>
2023-07-24 08:14:03 +0800

    Create Q3.sql

---
+++ Q3.sql
@@ -0,0 +1,24 @@
+## Q3 What is the Year-over-Year change in DESKTOP bookings for the month of September 2018 by order date? ##
+
+SELECT
+  yearmonth,
+  num_of_bookings,
+  num_of_bookings - LAG(num_of_bookings) OVER(ORDER BY yearmonth) as YoY_change_num,
+  ROUND((num_of_bookings - LAG(num_of_bookings) OVER(ORDER BY yearmonth))/num_of_bookings,4) AS YoY_change_pct
+FROM
+  (
+  SELECT
+    (EXTRACT(YEAR FROM OrderDate) || '-' || EXTRACT(MONTH FROM OrderDate)) AS yearmonth,
+    COUNT(ItineraryItemID) AS num_of_bookings
+  FROM Bookings t1
+  INNER JOIN DeviceType t2
+  ON t1.DeviceID = t2.DeviceID
+  WHERE 
+    t2.DeviceType = 'DESKTOP' AND
+    (
+      OrderDate BETWEEN '2018-09-01' AND '2018-09-30' OR
+      OrderDate BETWEEN '2017-09-01' AND '2017-09-30'
+    )
+  GROUP BY yearmonth
+  )
+ORDER BY yearmonth
